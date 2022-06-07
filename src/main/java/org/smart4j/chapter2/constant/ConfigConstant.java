bc862c49d164f51b8efe21f60413c258e025d2c4
yanwy <yanwy@asiainfo.com>
2022-06-07 18:04:51 +0800

    提交初始代码

---
+++ src/main/java/org/smart4j/chapter2/constant/ConfigConstant.java
@@ -0,0 +1,10 @@
+package org.smart4j.chapter2.constant;
+
+public interface ConfigConstant {
+    String CONFIG_FILE = "smart.properties";
+
+    String JDBC_DRIVER = "smart.framework.jdbc.driver";
+    String JDBC_URL = "smart.framework.jdbc.url";
+    String JDBC_USERNAME = "smart.framework.jdbc.username";
+    String JDBC_PASSWORD = "smart.framework.jdbc.password";
+}
